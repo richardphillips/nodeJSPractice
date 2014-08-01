396348ab42e46ed84216239fea0b67743b34b6a1
Richard Phillips <rphillips@tableausoftware.com>
2014-08-01 10:25:17 -0700

    Put server.js into a module added index.js

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 var http = require("http");
 
-http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("This is an http server");
-  response.end();
-}).listen(8888);
+function start() {
+  http.createServer(function(request, response) {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("This is an http server");
+    response.end();
+  }).listen(8888);
+}
+
+exports.start = start;

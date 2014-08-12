452286113b93b207912f30e35d6f0c7aa000888a
Richard Phillips <rphillips@tableausoftware.com>
2014-08-12 09:36:22 -0700

    fixed bug in requestHandlers

--- requestHandlers.js
+++ requestHandlers.js
@@ -3,11 +3,11 @@ var exec = require("child_process").exec;
 function start(response) {
   console.log("Request handler 'start' was called");
 
-  exec("ls -lah", function (error, stdout, stderr)) {
+  exec("ls -lah", function (error, stdout, stderr) {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("stdout");
+    response.write(stdout);
     response.end();
-  }
+  });
   
 }
 
